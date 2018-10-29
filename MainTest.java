f81d658a94e2858cf3fd58c4ec939b2c7cf2617e
gsanchez19 <42978523+gsanchez19@users.noreply.github.com>
2018-10-28 23:56:31 -0400

    Add files via upload

--- MainTest.java
+++ MainTest.java
@@ -6,22 +6,52 @@ public class MainTest
 	{
 		Dice die1 = new Dice();
 		Dice die2  = new Dice();
-		
-		int[] theDicer = new int[1000];
-		
-		int[] theCounter = new int[11];
+
+		int[] theCounter = new int[13];
 		
 		// boolean to indicate weather array has been filled or not.
 		
-		for(int i = 0; i < theDicer.length; i ++)
+		int doubleDice;
+		
+		theCounter[0] = 0;
+		theCounter[1] = 0;
+		theCounter[2] = 0;
+		theCounter[3] = 0;
+		theCounter[4] = 0;
+		theCounter[5] = 0;
+		theCounter[6] = 0;
+		theCounter[7] = 0;
+		theCounter[8] = 0;
+		theCounter[9] = 0;
+		theCounter[10] = 0;
+		theCounter[11] = 0;
+		theCounter[12] = 0;
+		
+		
+		for(int i = 0; i < 1000; i ++)
 		{
-			int doubleDice = die1.roll() + die2.roll();
-			theDicer[i] = doubleDice;
-			theCounter[doubleDice] = theCounter[doubleDice]
+			doubleDice = die1.roll() + die2.roll();
+			theCounter[(doubleDice)] = theCounter[doubleDice] + 1;
 			
 			
 		}
-			System.out.println("3 was rolled " + theCounter[2] + " times.");		
+			
+		int programCheck = theCounter[2] + theCounter[3] + theCounter[4] + theCounter[5] + theCounter[6];
+		programCheck = programCheck + theCounter [7] + theCounter[8] + theCounter[9] + theCounter[10] + theCounter[11] + theCounter[12];
+		
+			System.out.println("2 was rolled " + theCounter[2] + " times.");		
+			System.out.println("3 was rolled " + theCounter[3] + " times.");	
+			System.out.println("4 was rolled " + theCounter[4] + " times.");	
+			System.out.println("5 was rolled " + theCounter[5] + " times.");	
+			System.out.println("6 was rolled " + theCounter[6] + " times.");	
+			System.out.println("7 was rolled " + theCounter[7] + " times.");	
+			System.out.println("8 was rolled " + theCounter[8] + " times.");	
+			System.out.println("9 was rolled " + theCounter[9] + " times.");	
+			System.out.println("10 was rolled " + theCounter[10] + " times.");	
+			System.out.println("11 was rolled " + theCounter[11] + " times.");	
+			System.out.println("12 was rolled " + theCounter[12] + " times.");
+			
+			System.out.println("Program check. Times rolled: " + programCheck);
 			
 		
 		//while(notFilled)
